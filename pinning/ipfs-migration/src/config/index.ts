c12796c5972476f786cdda27360a645f646c8b6f
ravish1729 <ravish1729@gmail.com>
2024-05-27 15:58:18 +0530

    ipfs path fix

--- pinning/ipfs-migration/src/config/index.ts
+++ pinning/ipfs-migration/src/config/index.ts
@@ -12,7 +12,7 @@ const config = {
   redis_url: process.env.REDIS_URL ?? '',
   lighthouse_api_key: process.env.LIGHTHOUSE_API_KEY,
   logger_loki_host: process.env.LOGGER_LOKI_HOST ?? '',
-  ipfs_path: path.resolve('~/', 'ipfs/'),
+  ipfs_path: process.env.IPFS_PATH ?? 'ipfs/',
   peerID: process.env.PEER_ID ?? '',
   privKey: process.env.PRIVATE_KEY ?? '',
   pubKey: process.env.PUBLIC_KEY ?? '',
